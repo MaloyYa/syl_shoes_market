2a252a3863fe59ba92e95d5e173404c62c0458cb
Ivan Yakovlev <yaid9072@gmail.com>
2026-02-07 11:32:19 +0600

    mock files for UI testing

---
+++ src/mock/mockBestProducts.js
@@ -0,0 +1,41 @@
+export const bestProducts = [
+    {
+        title: 'КРОССОВКИ NIKE AIR MAX 270',
+        article: 'AM270-BLK',
+        price: 8990,
+        image: '/src/assets/images/product_2.svg',
+        isFavorite: true,
+    },
+    {
+        title: 'БЕГУНКИ PUMA RS-X REINVENT',
+        article: 'RSX-WHT',
+        price: 7490,
+        image: '/src/assets/images/product_3.svg',
+    },
+    {
+        title: 'СПОРТИВНЫЕ ТАПКИ ADIDAS SLIDE',
+        article: 'SLD-GRY',
+        price: 2490,
+        image: '/src/assets/images/product_2.svg',
+        isFavorite: true,
+    },
+    {
+        title: 'ТРЕНИРОВОЧНЫЕ КРОССОВКИ NEW BALANCE 574',
+        article: 'NB574-NVY',
+        price: 6990,
+        image: '/src/assets/images/product_4.svg',
+    },
+    {
+        title: 'МОДНЫЕ КЕДЫ CONVERSE CHUCK TAYLOR',
+        article: 'CT-RED',
+        price: 5490,
+        image: '/src/assets/images/product_2.svg',
+        isFavorite: true,
+    },
+    {
+        title: 'ЗИМНИЕ БОТИНКИ TIMBERLAND PREMIUM',
+        article: 'TB-WHT',
+        price: 12990,
+        image: '/src/assets/images/product_4.svg',
+    },
+];
---
+++ src/mock/mockBrands.js
@@ -0,0 +1,37 @@
+export const brands = [
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+    {
+        logoBrand: '/src/assets/images/puma_logo.svg',
+        titleBrand: 'Название',
+        url: '/',
+    },
+];
